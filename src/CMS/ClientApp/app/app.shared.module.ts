42dc037d43e4675b8ced56d7f7de2c031682ece3
William <williamlangroudi@gmail.com>
2017-12-13 12:43:53 +0200

    + pre npm update

---
+++ src/CMS/ClientApp/app/app.shared.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './components/app/app.component';
+import { HomeComponent } from './components/home/home.component';
+
+@NgModule({
+    declarations: [ // which components to use for the main app modeul
+        AppComponent,
+        HomeComponent
+    ],
+    imports: [
+        CommonModule,
+        HttpModule,
+        FormsModule,
+        RouterModule.forRoot([
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'home', component: HomeComponent },
+            { path: '**', redirectTo: 'home' }
+        ])
+    ]
+})
+export class AppModuleShared {
+}
