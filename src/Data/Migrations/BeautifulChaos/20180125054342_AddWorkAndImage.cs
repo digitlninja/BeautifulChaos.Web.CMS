e02efbf91d27be55d63fd3c12b2d5ff2216eb19a
William <williamlangroudi@gmail.com>
2018-01-25 07:45:07 +0200

    + added work and image + fixed migration

---
+++ src/Data/Migrations/BeautifulChaos/20180125054342_AddWorkAndImage.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Data.Migrations.BeautifulChaos
+{
+    public partial class AddWorkAndImage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Work",
+                columns: table => new
+                {
+                    PageId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    HeaderParagraph = table.Column<string>(nullable: true),
+                    HeaderTitle = table.Column<string>(nullable: true),
+                    WorkPageId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Work", x => x.PageId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    ImageId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AltText = table.Column<string>(nullable: true),
+                    Page = table.Column<string>(nullable: true),
+                    PageId = table.Column<int>(nullable: false),
+                    Title = table.Column<string>(nullable: true),
+                    Url = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.ImageId);
+                    table.ForeignKey(
+                        name: "FK_Images_Work_PageId",
+                        column: x => x.PageId,
+                        principalTable: "Work",
+                        principalColumn: "PageId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Images_PageId",
+                table: "Images",
+                column: "PageId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Images");
+
+            migrationBuilder.DropTable(
+                name: "Work");
+        }
+    }
+}
